95543e53e70c9a5ea6a2d240ef74cbe5bb8c444c
Shadowevil015 <64799983+Shadowevil015@users.noreply.github.com>
2025-01-13 16:19:19 +0000

    fix toast for no_auth check

--- src/app/login/ToastHandler.js
+++ src/app/login/ToastHandler.js
@@ -1,11 +1,13 @@
 'use client'
 
+import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 import { toast, ToastContainer } from "react-toastify"
 
 export default function ToastHandler({ message }) {
+    
     useEffect(() => {
-        if (message === 'auth-required') {
+        if (message === 'no_auth') {
             toast.warning('You need to be authenticated to access that page!')
         }
     }, [message])
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,14 +4,13 @@ import { LoginForm } from "../ui/login-form"
 import NavbarMain from "../ui/navbar/navbarMain"
 import ToastHandler from "./ToastHandler"
 
-export default async function Login({ searchParams }) {
+export default async function Login(props) {
+    const searchParams = await props.searchParams;
 
-    if (await verifySession() != undefined) {
+    if ((await verifySession()) != undefined) {
         redirect('/profile')
     }
     const message = searchParams?.message
-
-    console.log(message)
 
     return (
         <>
